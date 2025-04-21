1f047359e224b303fd4ba6567aeab51361054fb9
Holberton <flockwood@live.com>
2025-04-21 15:28:58 -0400

    created executor and path files

---
+++ executor.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * _fork - Forks a child process to run a command
+ * @argv: Array of command and its arguments
+ * Return: Exit status of the command
+ */
+
+int _fork(char **argv)
+{
+	pid_t pid;
+	int status;
+	char *full_path;
+
+	/* check if the command is an absolute or relative path */
+	if (access(argv[0], X_OK) == 0)
+	{
+		full_path = argv[0];
+	}
+	else
+	{
+		full_path = find_command_in_path(argv[0]);
+		if (!full_path)
+		{
+			write(STDERR_FILENO, argv[0], strlen(argv[0]));
+			write(STDERR_FILENO, ": command not found\n", 21);
+			return (127);
+		}
+	}
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		free(full_path != argv[0] ? full_path : NULL);
+		return (-1);
+	}
+	else if (pid == 0)
+	{
+		/* In child */
+		if (execve(full_path, argv, environ) == -1)
+		{
+			perror("execve");
+			exit(1);
+		}
+	}
+	else
+	{
+		/* In parent */
+		waitpid(pid, &status, 0);
+	}
+
+	if (full_path != argv[0])
+		free(full_path);
+
+	if (WIFEXITED(status))
+		return WEXITSTATUS(status);
+
+	return (-1);
+
+}
---
+++ path.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * get_full_path - Concatenates directory and command into full path
+ * @dir: Directory from PATH (e.g., /bin)
+ * @command: Command name (e.g., ls)
+ * Return: Full path (e.g., /bin/ls), must be freed by caller
+ */
+char *get_full_path(char *dir, char *command)
+{
+	char *full_path;
+	size_t len = strlen(dir) + strlen(command) + 2; /* +1 for '/' and +1 for '\0' */
+
+	full_path = malloc(len);
+	if (!full_path)
+		return (NULL);
+
+	sprintf(full_path, "%s/%s", dir, command);
+	return full_path;
+}
+
+/**
+ * find_command_in_path - Searches for a command in the PATH
+ * @command: The name of the command (e.g., "ls")
+ * Return: Full path to command if found, NULL otherwise
+ */
+char *find_command_in_path(char *command)
+{
+	char *path_env, *path_copy, *token, *full_path;
+	struct stat st;
+
+	path_env = getenv("PATH");
+	if (!path_env)
+		return (NULL);
+
+	path_copy = strdup(path_env);
+	if (!path_copy)
+		return (NULL);
+
+	token = strtok(path_copy, ":");
+	while (token)
+	{
+		full_path = get_full_path(token, command);
+		if (stat(full_path, &st) == 0 && (st.st_mode & S_IXUSR))
+		{
+			free(path_copy);
+			return full_path; /* Caller must free */
+		}
+		free(full_path);
+		token = strtok(NULL, ":");
+	}
+
+	free(path_copy);
+	return (NULL);
+}
+
